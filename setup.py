9e1a36bba7a654c390e30aa8296de6f91e13c94a
LyroxCoder <japonase613@gmail.com>
2026-04-23 13:40:36 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="LyroxCoder",
+    version="2.2.3",
+    py_modules=["LyroxCoder"],
+    install_requires=["base91"],
+    author="LyroxCoder",
+    description="Telegram; @LyroxPy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
